d3d77a7b38d08311ee4ddc48cd36be5268d69ded
anand.sankaran <anand.sankaran@workday.com>
2020-06-28 20:43:39 -0700

    Hospital data

--- alameda_data.rb
+++ alameda_data.rb
@@ -1,6 +1,7 @@
 require 'net/http'
 require 'uri'
 require 'json'
+require 'date'
 
 # Alameda county case data and hospital data JSON
 CASE_DATA_JSON = 'https://opendata.arcgis.com/datasets/f6195a2de0b0440c99ceb9290fa95316_0.geojson'
@@ -29,16 +30,29 @@ end
 
 # Alameda county hospital data is not updated daily here.  
 
-# escaped_address = URI.escape(HOSPITAL_DATA_JSON) 
-# uri = URI.parse(escaped_address)
+escaped_address = URI.escape(HOSPITAL_DATA_JSON) 
+uri = URI.parse(escaped_address)
 
-# json = Net::HTTP.get(uri)
-# result = JSON(json)
-# features = result['features']
+json = Net::HTTP.get(uri)
+result = JSON(json)
+features = result['features']
 
-# hospital_data = {}
+hospital_data = {}
 
-# features.each do |feature|
-#     properties = feature['properties']
-#     p properties.inspect
-# end
+features.each do |feature|
+    properties = feature['properties']
+    date = properties['Date']
+    total_positive = properties['Hospitalized_COVID_19_Positive_']
+    icu = properties['ICU_COVID_19_Positive_Patients']
+    d = Date.parse(date)
+    hospital_data[d] = {'total' => total_positive, 'icu' => icu}
+end
+
+result = hospital_data.sort.reverse
+last_hospital_info = result[0]
+last_date = last_hospital_info[0]
+last_info = last_hospital_info[1]
+
+puts "\n"
+p "Last available hospital information:"
+p "#{last_date.year}/#{last_date.month}/#{last_date.day} : #{last_info['total']} : #{last_info['icu']}"
